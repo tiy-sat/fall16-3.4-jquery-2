1925134ec8bf3e875e5824dbed39b4a8eb3b378f
Jason Vanderslice <jlvanderslice@gmail.com>
2016-12-15 11:14:32 -0600

    Created vars in js to reference elements needed for upcoming functionality

--- main.js
+++ main.js
@@ -2,5 +2,12 @@
 
 $(function(){
   // All code relating to the DOM
+  var $body = $("body");
+
+  // By using the method `find()` we are saving some memory (faster in browser)
+  var $textarea = $body.find("[data-js='postForm__text']");
+  var $submitButton = $body.find("[data-js='postForm__submit']");
+
   
+
 });
