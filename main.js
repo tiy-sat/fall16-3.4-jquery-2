e7390d3cc88cfe1c40a8db55c54406266b96055e
Jason Vanderslice <jlvanderslice@gmail.com>
2016-12-15 11:16:28 -0600

    Created event listener for posting and pseudo coded upcoming tasks

--- main.js
+++ main.js
@@ -8,6 +8,11 @@ $(function(){
   var $textarea = $body.find("[data-js='postForm__text']");
   var $submitButton = $body.find("[data-js='postForm__submit']");
 
-  
+  $submitButton.on("click", function(e){
+    // Inside of here... we know the submit button has been clicked
+    // Take the textarea value
+    // Create a new article with the value from the text area
+    // Prepend new article template to list of articles
+  });
 
 });
